fbf4a9459d74ea8fa6d929552a9ce8b342c6d4c9
ivansanchezvera <ivansanchez_1988@hotmail.com>
2017-07-17 17:07:23 -0500

    Agregado metodo jugar para simular jugabilidad. Agregada Interfaz enemigo y metodo generar enemigo con clase local de ejemplo que implementa enemigo

--- src/Juego.java
+++ src/Juego.java
@@ -1,9 +1,15 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Juego {
 
+	interface Enemigo{
+		public void atacarJugador(Jugador j);
+		public int getPoderEnemigo();
+	}
+	
 	static class PersonajeJuego{
 		private String nombrePersonaje;
 		private int defensa;
@@ -165,6 +171,134 @@ public class Juego {
 	public void agregarArena(Arena arena)
 	{
 		arenasDisponibles.add(arena);
+	}
+	
+	public void jugar()
+	{
+		Random r = new Random();
+		Enemigo e = generarEnemigo();
+		
+		do{
+			int turno = r.nextInt(2);
+			
+			if(turno==0)
+			{
+				atacarDefender(jugadorA, jugadorB, arena);
+			}else{
+				atacarDefender(jugadorB, jugadorA, arena);
+			}
+			
+			int malaSuerte = r.nextInt(3);
+			switch (malaSuerte) {
+				case 0: break;
+				case 1: e.atacarJugador(jugadorA);
+					break;
+				case 2: e.atacarJugador(jugadorB);
+					break;
+			}
+			
+		}while(jugadorA.getSalud()>0 && jugadorB.getSalud()>0);
+		
+		Jugador ganador = ( jugadorA.getSalud()>0 ? jugadorA : jugadorB);
+		System.out.println("El ganador es ... " + ganador.getNombreJugador());
+	}
+
+	public void atacarDefender(Jugador atacante, Jugador defensor, Arena arena) {
+		Random r = new Random();
+		
+		System.out.println("*********Batalla Salvaje***********");
+		System.out.println(atacante.getNombreJugador() + " vs " + defensor.getNombreJugador());
+		System.out.println("Estadisticas:");
+		System.out.println("Atacante : " + atacante);
+		System.out.println("Defensor : " + defensor + "\n");
+		System.out.println("Comienza batalla.....");
+		int poderTotalAtaqueP1 = atacante.getPersonajeElegido().getPoder();
+		int poderRealAtaqueP1 = poderTotalAtaqueP1 * r.nextInt(100)/100;
+		
+	
+		int defensaTotalP2 = defensor.getPersonajeElegido().getDefensa();
+		int defensaRealP2 = defensaTotalP2 * r.nextInt(100)/100;
+		
+		switch (arena.tipo){
+		case OFENSIVA:
+			poderRealAtaqueP1 = poderRealAtaqueP1*2;
+			defensaRealP2 = Math.round( (float) (defensaRealP2 * 0.5));
+			break;
+		
+		case DEFENSIVA:
+			defensaRealP2 = defensaRealP2*2;
+			poderRealAtaqueP1 = Math.round( (float) (poderRealAtaqueP1 * 0.5));
+			break;
+		
+		case NEUTRA: break;
+		}
+		
+		System.out.println(atacante.getNombreJugador() + " dice: toma esto @#$@#!. Ataca con" + poderRealAtaqueP1);
+		//System.out.println("Jugador " + atacante.getNombreJugador() + " ataca con " + poderRealAtaqueP1 );
+		System.out.println("Jugador " + defensor.getNombreJugador() + " se defiende con " + defensaRealP2);
+		int danoRealizado = (poderRealAtaqueP1-defensaRealP2>0 ? poderRealAtaqueP1-defensaRealP2 : 0);
+		int saludActualP2 = defensor.getSalud() - danoRealizado;
+		defensor.setSalud(saludActualP2);
+		
+		if(danoRealizado>0)
+		{
+			System.out.println("Oh no, me has danado! ... nueva salud de " + defensor.getNombreJugador() + " es " + defensor.getSalud());
+		}else {
+			System.out.println("Con quien crees que te estas metiendo! ... " + defensor.getNombreJugador() + " se ha defendido bien y su salud esta intacta!" + defensor.getSalud() );
+		}
+		
+		System.out.println("*******************************\n\n");
+	}
+
+	public Jugador getJugadorA() {
+		return jugadorA;
+	}
+
+	public void setJugadorA(Jugador jugadorA) {
+		this.jugadorA = jugadorA;
+	}
+
+	public Jugador getJugadorB() {
+		return jugadorB;
+	}
+
+	public void setJugadorB(Jugador jugadorB) {
+		this.jugadorB = jugadorB;
+	}
+
+	public Arena getArena() {
+		return arena;
+	}
+
+	public void setArena(Arena arena) {
+		this.arena = arena;
+	}
+
+	public LocalDateTime getFechaPartida() {
+		return fechaPartida;
+	}
+	
+	public Enemigo generarEnemigo(){
+		class EnemigoSimplon implements Enemigo{
+
+			int poder = 10; 
+			
+			@Override
+			public void atacarJugador(Jugador j) {
+				j.setSalud(j.getSalud()-poder);
+				System.out.println(j.getNombreJugador() + " es super salado. El enemigo simplon a atacado a " + j.getNombreJugador() 
+				+ ". Nueva salud de " + j.getNombreJugador() + " es " + j.getSalud());
+			}
+
+			@Override
+			public int getPoderEnemigo() {
+				
+				return poder;
+			}
+		}
+		
+		Enemigo e = new EnemigoSimplon();
+		return e;
 	}
 	
 	
--- src/SimuladorJuego.java
+++ src/SimuladorJuego.java
@@ -7,17 +7,31 @@ public class SimuladorJuego {
 	public static void main(String[] args) {
 		
 		Juego.PersonajeJuego miniPekka = new Juego.PersonajeJuego("miniPekka", 70, 30);
-		Juego.PersonajeJuego dragonInfernal = new Juego.PersonajeJuego("Dragon Infernal", 25, 10);
-		Juego.PersonajeJuego bandida = new Juego.PersonajeJuego("Bandida", 90, 60);
+		Juego.PersonajeJuego dragonInfernal = new Juego.PersonajeJuego("Dragon Infernal", 70, 40);
+		Juego.PersonajeJuego bandida = new Juego.PersonajeJuego("Bandida", 90, 15);
 		
 		Juego.Arena legendario = new Juego.Arena("Arena Legendaria", Juego.Arena.TipoArena.OFENSIVA, 10);
 		Juego.Arena montaPuertos = new Juego.Arena("Monta Puertos", Juego.Arena.TipoArena.DEFENSIVA, 20);
 		
 		Juego partidaMortal = new Juego(); 
 		
-		Juego.Jugador ivan = partidaMortal.new Jugador("Manuelito", 100, bandida);
+		Juego.Jugador manuelito = partidaMortal.new Jugador("Manuelito", 100, bandida);
 		Juego.Jugador abel = partidaMortal.new Jugador("abel", 100, dragonInfernal);
 		
+		//Agrego una Arena
+		partidaMortal.agregarArena(montaPuertos);
+		partidaMortal.agregarArena(legendario);
+		
+		//Agrego personajes, no es totalmente necesario
+		partidaMortal.agregarPersonaje(bandida);
+		partidaMortal.agregarPersonaje(dragonInfernal);
+		partidaMortal.agregarPersonaje(miniPekka);
+		
+		partidaMortal.setArena(montaPuertos);
+		partidaMortal.setJugadorA(manuelito);
+		partidaMortal.setJugadorB(abel);
+		
+		partidaMortal.jugar();
 	}
 
 }
